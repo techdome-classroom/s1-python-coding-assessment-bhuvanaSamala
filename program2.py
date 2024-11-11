0b267b1b4ebb08e6f5bae08a5a2d8cd713de3be0
bhuvanaSamala <bhuvanasamala7@gmail.com>
2024-11-11 13:56:24 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -2,6 +2,7 @@ def decode_message( s: str, p: str) -> bool:
 
 # write your code herem = len(s)
     n = len(p)
+    m=len(s)
 
     dp = [[False] * (n + 1) for _ in range(m + 1)]
     dp[0][0] = True
