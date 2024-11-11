e95b6d2e6927e77296c74c6a4c2606c688efbd97
bhuvanaSamala <bhuvanasamala7@gmail.com>
2024-11-11 13:50:15 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,9 +2,6 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here def dfs(i, j):     
-        
-class Solution:
-    def getTotalIsles(self, grid: list[list[str]]) -> int:
         def dfs(i, j):
             if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == 'W':
                 return
